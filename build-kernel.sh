f0f1b349750b643ac1a63bceb551c769e784d231
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-08-15 17:46:17 +0900

    Update git clone command in build-kernel.sh
    Removed specific branch reference for cloning Linux kernel.

--- build-kernel.sh
+++ build-kernel.sh
@@ -29,7 +29,7 @@ fi
 cd $linux_dir
 #git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b linux-7.1.y
 #git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b v7.0.10
- git clone --depth 1 https://github.com/torvalds/linux.git -b v7.2-rc7
+ git clone --depth 1 https://github.com/torvalds/linux.git
 
 mkdir minimyth2 && cd minimyth2
 # 3559-media-rkvdec-fix-PM-runtime-teardown-ordering-in-remove.patch
